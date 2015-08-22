142de297071621f5a2d3cece6cf73877b701076a
Max <miramaxis@gmail.com>
2015-08-22 18:27:12 +0300

    MapsGeFacade added

--- MyMapsGeTypeScript/ge/mymaps/map/MapsGeFacade.ts
+++ MyMapsGeTypeScript/ge/mymaps/map/MapsGeFacade.ts
@@ -1,7 +1,21 @@
-﻿class MapsGeFacade
-{
-    constructor(width: number, height: number)
-    {
+﻿
+@component("maps-ge-facade")
+class MapsGeFacade extends polymer.Base implements polymer.Element {
+    attached() {
 
+        window['gmloaded'] = function () {
+            console.log('google maps loaded');
+
+            console.log('web components ready ');
+            var map: TbilisiMapView = <any>document['getElementById']('mapView');
+            map.initialize();
+            var list: MapTypeList  = <any>document.getElementById('typesList');
+
+            list.setMapTypesProvider(map.mapTypesProvider);
+
+            /*var list = document.getElementById('list');
+            list.mapTypesProvider = map.mapTypesProvider;*/
+        }
     }
-} 
+}
+MapsGeFacade.register(); 
